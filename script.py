bc407845aaf19f2f9bba0fa0d82af7a2420f9826
Oliveira <marco.oliveira@sap.com>
2020-03-05 15:10:04 -0800

    fixes

--- script.py
+++ script.py
@@ -9,11 +9,11 @@ class TestAcademySite(unittest.TestCase):
 
     def test_academy_site(self):
         driver = self.driver
-        driver.get('https://www.google.com/search?q=sap+academy+engineering&rlz=1C1GCEU_pt-BRBR819BR819&oq=sap+aca&aqs=chrome.0.69i59l2j69i60j69i61j69i60j69i65l3.1025j0j7&sourceid=chrome&ie=UTF-8')
-        link = driver.find_element_by_xpath('//a[@href="'+'https://sapengineering.academy/'+'"]')
-        link.click()
+        driver.get('https://sapengineering.academy/')
         self.assertIn('SAP Academy for Engineering – Beyond Code', driver.title)
+        self.assertIn('https://sapengineering.academy/', driver.current_url)
         assert "404 - No results" not in driver.page_source
+
     def test_write_in_google(self):
         driver = self.driver
         driver.get('https://www.google.com/')
@@ -23,8 +23,8 @@ class TestAcademySite(unittest.TestCase):
         link = driver.find_element_by_xpath('//a[@href="'+'https://sapengineering.academy/'+'"]')
         link.click()
         self.assertIn('SAP Academy for Engineering – Beyond Code', driver.title)
+        self.assertIn('https://sapengineering.academy/', driver.current_url)
         assert "404 - No results" not in driver.page_source
-
 
     def tearDown(self):
         self.driver.close()
